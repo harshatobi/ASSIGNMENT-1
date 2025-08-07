d3723cb1efa8c805d876731ec41f3d818d752187
harshatobi <harshapayala@gmail.com>
2025-08-07 23:28:16 +0530

    Add files via upload

---
+++ TASK 2.py
@@ -0,0 +1,5 @@
+#TASK 2
+first_name=input("Enter your first name : ")
+second_name=input("Enter your second name : " )
+full_name=first_name+" "+second_name
+print("Hello, "+full_name+ "! Welcome to the python program.")
